b4c7e615223aa56e6decd12ac941140d126e812c
HuzaifaIrfan <huzaifairfan2001@gmail.com>
2025-07-09 15:53:48 +0500

    create_stair_stringer

---
+++ stair_stringer_cnc.py
@@ -0,0 +1,8 @@
+
+from stair_stringer import create_stair_stringer
+
+def main():
+    create_stair_stringer(doc_name="stair_stringer_cnc", stringer_thickness = 1.5, first_rise_height = 6.62, rise_height = 7.625,num_rise = 16, run_depth = 11.5, num_run = 15, kicker_depth = 5.5, kicker_height = 1.5, stair_angle=33.5, back_cut_increase=5, rotate_for_cnc=True)
+
+if __name__=="__main__":
+    main()

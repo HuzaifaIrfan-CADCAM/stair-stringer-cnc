4f72311c88ce5f14c09e9de93f054db1247221bd
HuzaifaIrfan <huzaifairfan2001@gmail.com>
2025-07-09 21:23:07 +0500

    FreecadDoc class

--- freecad_doc/stair_stringer/__init__.py
+++ freecad_doc/stair_stringer/__init__.py
@@ -1,6 +1,12 @@
 
 import freecad_env
 
+import FreeCAD, Part
+
+import FreeCAD as App
+import Part, PartDesign, Sketcher
+from FreeCAD import Vector
+
 
 import math
 
@@ -8,7 +14,7 @@ def inch_to_mm(inches: float) -> float:
     """Convert inches to millimeters."""
     return inches * 25.4
 
-def create_stair_stringer(doc_name, stringer_thickness, first_rise_height, rise_height, num_rise, run_depth, num_run, kicker, kicker_depth, kicker_height, stair_angle, back_cut_increase, rotate_for_cnc):
+def create_stair_stringer(doc, body_name, stringer_thickness, first_rise_height, rise_height, num_rise, run_depth, num_run, kicker, kicker_depth, kicker_height, stair_angle, back_cut_increase, rotate_for_cnc):
     
 
     # Parameters
@@ -49,22 +55,12 @@ def create_stair_stringer(doc_name, stringer_thickness, first_rise_height, rise_
     first_bottom_run_depth=run_depth+inch_to_mm(back_cut_increase)
     last_reverse_rise_height=rise_height+inch_to_mm(back_cut_increase)
 
-    import FreeCAD, Part
 
-    import FreeCAD as App
-    import Part, PartDesign, Sketcher
-    from FreeCAD import Vector
 
-    # doc_name="StairStringerFixed"
+    body=doc.addObject('PartDesign::Body',body_name)
+    body.Label = body_name
 
-    doc = App.newDocument(doc_name)
-    App.setActiveDocument(doc_name)
-    App.ActiveDocument=App.getDocument(doc_name)
-
-    body=doc.addObject('PartDesign::Body','Body')
-    body.Label = 'Body'
-
-    sketch=body.newObject('Sketcher::SketchObject','Sketch')
+    sketch=body.newObject('Sketcher::SketchObject','Stringer_Sketch')
     sketch.AttachmentSupport = (doc.getObject('XY_Plane'),[''])
     sketch.MapMode = 'FlatFace'
 
@@ -251,34 +247,31 @@ def create_stair_stringer(doc_name, stringer_thickness, first_rise_height, rise_
 
     doc.recompute()
 
+    return body
 
-    import PathScripts
-    import Path
-    import Path.Base.Gui as PathGui
-    import Path.Main.Job as PathJob
-    import Path.Post.Processor as PostProcessor
-    import Path.Tool as PathTool
-    import Path.Op.Profile as PathProfile
 
+import PathScripts
+import Path
+import Path.Base.Gui as PathGui
+import Path.Main.Job as PathJob
+import Path.Post.Processor as PostProcessor
+import Path.Tool as PathTool
+import Path.Op.Profile as PathProfile
+
+def create_job(doc, body, job_name="Job"):
     
-
-    job = PathJob.Create("Job", [body]) 
+    job = PathJob.Create(job_name, [body]) 
     print(job)
     PathGui.ActiveJob = job
 
     doc.recompute()
 
 
-
-    profile=PathProfile.Create("cnc_profile")
-
-
+    profile=PathProfile.Create(f"{job_name}_profile")
 
 
     doc.recompute()
 
-
-    
 
     print("allAvailablePostProcessors")
     print(Path.Preferences.allAvailablePostProcessors())
@@ -286,93 +279,9 @@ def create_stair_stringer(doc_name, stringer_thickness, first_rise_height, rise_
 
     # Generate GCode
     postprocessor = PostProcessor.PostProcessorFactory.get_post_processor(job,"linuxcnc")
-    gcode = postprocessor.export()
+    gcode = postprocessor.export()[0][1]
+
     print(gcode)
 
-
-
-
-
-
-  
-    import os
-
-    current_directory = os.getcwd()
-    print("Current Working Directory:", current_directory)
-
-    file_name=doc_name
-    output_folder=f"{current_directory}/output/"
-    os.makedirs(output_folder, exist_ok=True)
-
-
-
-    # Save GCode
-    cnc_file_name = f"{output_folder}/{file_name}.nc"
-    with open(cnc_file_name, "w") as f:
-        f.write(gcode[0][1])
-    print(f"GCode successfully generated at {cnc_file_name}")
-
-
-
-    doc_file_name=f"{output_folder}/{file_name}.FCStd"
-    doc.saveAs(doc_file_name)
-    print(f"doc_file_name: {doc_file_name}")
-
-    __objs__ = []
-    __objs__.append(body)
-    import importers.importOBJ
-
-    obj_file_name=f"{output_folder}/{file_name}.obj"
-    print(f"obj_file_name: {obj_file_name}")
-    if hasattr(importers.importOBJ, "exportOptions"):
-        options = importers.importOBJ.exportOptions(obj_file_name)
-        importers.importOBJ.export(__objs__,obj_file_name, options)
-    else:
-        importers.importOBJ.export(__objs__, obj_file_name)
-
-    del __objs__
-
-
-    __objs__ = []
-    __objs__.append(body)
-    import Mesh
-    stl_file_name=f"{output_folder}/{file_name}.stl"
-    print(f"stl_file_name: {stl_file_name}")
-    if hasattr(Mesh, "exportOptions"):
-        options = Mesh.exportOptions(stl_file_name)
-        Mesh.export(__objs__, stl_file_name, options)
-    else:
-        Mesh.export(__objs__, stl_file_name)
-
-    del __objs__
-
-
-    __objs__ = []
-    __objs__.append(body)
-    import importDXF
-    dxf_file_name=f"{output_folder}/{file_name}.dxf"
-    print(f"dxf_file_name: {dxf_file_name}")
-    if hasattr(importDXF, "exportOptions"):
-        options = importDXF.exportOptions(dxf_file_name)
-        importDXF.export(__objs__, dxf_file_name, options)
-    else:
-        importDXF.export(__objs__, dxf_file_name)
-
-    del __objs__
-
-
-    # __objs__ = []
-    # __objs__.append(body)
-
-    # import ImportGui
-    # step_file_name=f"{output_folder}/{file_name}.step"
-    # print(f"step_file_name: {step_file_name}")
-    # if hasattr(ImportGui, "exportOptions"):
-    #     options = ImportGui.exportOptions(step_file_name)
-    #     ImportGui.export(__objs__, step_file_name, options)
-    # else:
-    #     ImportGui.export(__objs__, step_file_name)
-
-    # del __objs__
-
+    return gcode
 
